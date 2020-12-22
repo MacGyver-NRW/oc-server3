046db15cdd334b5fed1d00707fb27487d8d5be40
MacGyver-NRW <clanfamily.nrw@gmail.com>
2020-12-22 23:21:40 +0100

    OCC-83 Add Stylelinter rules & Optimize some Files

--- htdocs_symfony/assets/app/oc-style.js
+++ htdocs_symfony/assets/app/oc-style.js
@@ -1,14 +1,14 @@
 /*!
  * Opencaching working with Symfony4 webpack encore
- * See this tutrorial and learn how it works:
+ * See this tutorial and learn how it works:
  * https://symfonycasts.com/screencast/webpack-encore
  */
 
 // Starting with our APP Code
 import './styles/oc-style.scss';
 
-// Import thrid pary packagist
+// Import third party packagist
 import $ from 'jquery';
 import 'bootstrap';
 
-console.log('Loaded OC4 entrypoint');
+console.log('Loaded OC-Style entrypoint');
